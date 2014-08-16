e751f9db7db5401236443495b0c199c95f602e73
Kassymkhan Kapparov <kassymkhan@gmail.com>
2014-08-16 10:58:04 +0000

    Signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
   
+  get 'users/show'
 
+  match '/signup', to: "users#new", via: :get
+  
   root 'static_pages#home'
   
   match '/about', to: 'static_pages#about', via: :get
   match '/help', to: 'static_pages#help', via: :get
+  match '/contact', to: 'static_pages#contact', via: :get
+  
   
   resources :microposts
   resources :users
