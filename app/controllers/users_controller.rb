369f2cddbcbeb71358231c2b0bec6ca01cdffcc7
Kassymkhan Kapparov <kassymkhan@gmail.com>
2014-08-19 10:16:41 +0000

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,33 +1,62 @@
 class UsersController < ApplicationController
- 
-  def new
-    @user = User.new
-  end
-
+  before_action :signed_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+  
+  
   def show
     @user = User.find(params[:id])
   end
   
+  def new
+    @user = User.new
+  end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "Пользователь удален."
+    redirect_to users_url
+  end
+  
   def create
     @user = User.new(user_params)
-  
     if @user.save
-     
-      flash[:success] = "Привет #{@user.name.capitalize}, вэлкам, твоя регистрация прошла на ура!"
-    
-      # process and save to db
+      sign_in @user
+      flash[:success] = "Добро пожаловат в Twitter!"
       redirect_to @user
     else
-      # show mistake
       render 'new'
     end
-    
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Профиль обновлен."
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
   
   private
-  def user_params
-    params.require(:user).permit(:name, :email, :password,
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
-  end
+    end
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Пожалуйста войдите."
+      end
+    end
+  
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
   
 end
