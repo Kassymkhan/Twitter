788d071993f8b6a7c028c968ca49d925320b7770
Kassymkhan Kapparov <kassymkhan@gmail.com>
2014-08-19 10:57:30 +0000

    Translation to russian

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       sign_in @user
-      flash[:success] = "Добро пожаловат в Twitter!"
+      flash[:success] = "Добро пожаловать в Twitter!"
       redirect_to @user
     else
       render 'new'
