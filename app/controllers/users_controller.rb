b818a4a6c3561e0d645e656c5b650c327ce1d2c7
Kassymkhan Kapparov <kassymkhan@gmail.com>
2014-08-21 10:07:26 +0000

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,58 +1,66 @@
 class UsersController < ApplicationController
-  before_action :signed_in_user, only: [:edit, :update]
+  
+  before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
+  
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+ 
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Данные успешно обновлены!"
+      redirect_to @user
+    else
+      render 'edit'
+    end    
+  end
   
   
   def show
     @user = User.find(params[:id])
+   # @microposts = @user.microposts.paginate(page: params[:page])
   end
   
   def new
     @user = User.new
   end
   
-  def destroy
-    User.find(params[:id]).destroy
-    flash[:success] = "Пользователь удален."
-    redirect_to users_url
-  end
-  
   def create
     @user = User.new(user_params)
     if @user.save
       sign_in @user
-      flash[:success] = "Добро пожаловать в Twitter!"
+      flash[:success] = "Welcome to Twitter!"
       redirect_to @user
     else
       render 'new'
     end
   end
   
-  def edit
+def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted."
+    redirect_to users_url
   end
   
-  def update
-    if @user.update_attributes(user_params)
-      flash[:success] = "Профиль обновлен."
-      redirect_to @user
-    else
-      render 'edit'
-    end
-  end
   
   private
+  
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
 
-    def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_url, notice: "Пожалуйста войдите."
-      end
-    end
   
     def correct_user
       @user = User.find(params[:id])
